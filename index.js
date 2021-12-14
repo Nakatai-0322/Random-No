35b4bde445c6289f867d032542ec6ac694c9e3a0
Nakatai-0322 <nakatai0322@gmail.com>
2021-12-14 19:18:25 +0900

    Update index.js
    数値化処理要らんかった。

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function RandomNumber() {
-    let max = Number(document.getElementById("maxn").value)
+    let max = document.getElementById("maxn").value
     let n = chance.natural({min: 0 , max: max});
     document.getElementById("out").innerHTML = n;
     console.log(n)
