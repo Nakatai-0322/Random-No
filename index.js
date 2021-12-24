c3da641ca337f371ede3a32d7b26181680c52e9b
Nakatai-0322 <nakatai0322@gmail.com>
2021-12-24 21:07:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+let max = document.getElementById("maxn").value;
+let n = chance.natural({min: 0 , max: max});
+
 function RandomNumber() {
-    let max = document.getElementById("maxn").value
-    let n = chance.natural({min: 0 , max: max});
     document.getElementById("out").innerHTML = n;
     console.log(n)
 }
