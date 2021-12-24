e4178fe67246951c67bc34a10ccf7c99b4d2fb7f
Nakatai-0322 <nakatai0322@gmail.com>
2021-12-24 21:16:36 +0900

    Revert "Update index.js"
    This reverts commit c3da641ca337f371ede3a32d7b26181680c52e9b.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-let max = document.getElementById("maxn").value;
-let n = chance.natural({min: 0 , max: max});
-
 function RandomNumber() {
+    let max = document.getElementById("maxn").value
+    let n = chance.natural({min: 0 , max: max});
     document.getElementById("out").innerHTML = n;
     console.log(n)
 }
