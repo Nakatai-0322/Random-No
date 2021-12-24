e2b2a91a65faf44db8248de4e52143729a4c5686
Nakatai-0322 <nakatai0322@gmail.com>
2021-12-25 08:29:25 +0900

    add min

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function RandomNumber() {
+    let min = document.getElementById("minn").value
     let max = document.getElementById("maxn").value
-    let n = chance.natural({min: 0 , max: max});
+    let n = chance.natural({min: min , max: max});
     document.getElementById("out").innerHTML = n;
     console.log(n)
 }
