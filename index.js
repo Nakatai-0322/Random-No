e49949aa274a397cfe7f0e855e462d968cd9208a
Nakatai <info@nakatai.ga>
2021-12-31 09:43:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+Random-No
+
+Copyright (c) 2021~Present Nakatai.
+
+This software is released under the MIT License.
+https://opensource.org/licenses/mit-license.php
+*/
 function RandomNumber() {
     let min = document.getElementById("minn").value
     let max = document.getElementById("maxn").value
