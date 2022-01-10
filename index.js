056ecb6f4c1d15bfb7cf31cd04e4bc968c7aa40b
Nakatai-0322 <nakatai0322@gmail.com>
2022-01-10 18:11:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ https://opensource.org/licenses/mit-license.php
 function RandomNumber() {
     let min = document.getElementById("minn").value
     let max = document.getElementById("maxn").value
-    let Randon = chance.natural(
-        {min: min , max: max}
-    );
+    let Randon = chance.natural({min: min , max: max});
     document.getElementById("out").innerHTML = Randon;
     console.log(Randon)
 }
