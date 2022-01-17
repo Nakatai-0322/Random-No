e771b3da54df0c84a3230933989e784df0f3b7ec
Nakatai-0322 <nakatai0322@gmail.com>
2022-01-17 17:14:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ This software is released under the MIT License.
 https://opensource.org/licenses/mit-license.php
 */
 function RandomNumber() {
-    const min = document.getElementById("minn").value
-    const max = document.getElementById("maxn").value
-    const Randomn = chance.natural({min: min , max: max});
+    const min = document.getElementById("minn").value;
+    const max = document.getElementById("maxn").value;
+    const Randomn = chance.natural({min: min, max: max});
     document.getElementById("out").innerHTML = Randomn;
-    console.log(Randomn)
+    console.log(Randomn);
 }
