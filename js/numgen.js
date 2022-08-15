a78b03947e12004992d9991d704674889accb322
Nakatai-0322 <nakatai0322@gmail.com>
2022-08-15 19:38:05 +0900

    4 spaces to tab

--- js/numgen.js
+++ js/numgen.js
@@ -15,28 +15,28 @@ let HistoryNum;
 let NewHistory;
 
 function RandomNumber() {
-    const minn = Number(document.getElementById("minn").value);
-    const maxn = Number(document.getElementById("maxn").value);
-    const Randomn = chance.natural({
-        min: minn,
-        max: maxn
-    });
-    document.getElementById("out").innerHTML = Randomn;
-    exporttoHTML(Randomn);
-    console.log(Randomn);
-    if (HistoryNum == null || undefined) {
-        HistoryNum = document.createElement("div");
-    };
+	const minn = Number(document.getElementById("minn").value);
+	const maxn = Number(document.getElementById("maxn").value);
+	const Randomn = chance.natural({
+		min: minn,
+		max: maxn
+	});
+	document.getElementById("out").innerHTML = Randomn;
+	exporttoHTML(Randomn);
+	console.log(Randomn);
+	if (HistoryNum == null || undefined) {
+		HistoryNum = document.createElement("div");
+	};
 };
 
 function exporttoHTML(arg) {
-    HistoryNum = document.getElementById("HistoryNum");
-    const new_element = document.createElement("code");
-    const txt = arg + " ";
-    new_element.textContent = txt;
-    HistoryNum.appendChild(new_element);
-    if (boolofcons1 === false){
-        document.getElementById("footer-1").remove();
-        boolofcons1 = true;
-    };
+	HistoryNum = document.getElementById("HistoryNum");
+	const new_element = document.createElement("code");
+	const txt = arg + " ";
+	new_element.textContent = txt;
+	HistoryNum.appendChild(new_element);
+	if (boolofcons1 === false){
+		document.getElementById("footer-1").remove();
+		boolofcons1 = true;
+	};
 };
