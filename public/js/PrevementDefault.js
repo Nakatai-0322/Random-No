795265c10184fb781d468890fada2edd388df4b0
Nakatai-0322 <nakatai0322@gmail.com>
2022-10-31 20:52:23 +0900

    Create PrevementDefault.js

---
+++ public/js/PrevementDefault.js
@@ -0,0 +1,13 @@
+/*
+Random-No
+
+Copyright (c) 2022~Present Nakatai.
+
+This software is released under the Apache License.
+https://opensource.org/licenses/Apache-2.0
+*/
+const inputElements = document.getElementsByTagName("input");
+
+inputElements.addEventListener("click", () => {
+	event.preventDefault();
+});
